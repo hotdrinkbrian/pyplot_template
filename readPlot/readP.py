c3d2b13ab6cf30594f1510f079be936aa3f7e47d
hotdrinkbrian <hotdrink1104@gmial.com>
2018-11-01 16:04:16 +0100

    save

---
+++ readPlot/readP.py
@@ -0,0 +1,91 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from hm import *
+
+#path = '../qcd_with_jumps/'
+path = '../'
+#pklN = 'result_with_pt_mass_energy.pickle'
+pklN = 'result_v0.pickle'
+
+#attrL = ['cHadE','nHadE','cHadEFrac','nHadEFrac','nEmE','nEmEFrac','cEmE','cEmEFrac','cmuE','cmuEFrac','muE','muEFrac','eleE','eleEFrac','eleMulti','photonE','photonEFrac','photonMulti','cHadMulti','npr','cMulti','nMulti']
+attrL=['cHadE','nHadE']
+
+
+f1 = open(path + pklN,'read')
+ret = pickle.load(f1)
+#print ret
+#for key in ret:
+#    print key
+maxAOC = 0
+maxKey = ''
+for key in ret:
+    tempAOC = ret[key]['aoc']
+    if tempAOC > maxAOC:
+        maxAOC = tempAOC
+        maxKey = key 
+    #print key + ': ' + str( ret[key]['aoc'] )
+print 'optimal combi: ' + maxKey
+print 'AOC: '+ str( maxAOC )
+#print '~~~~~~~~~~~~~~~~~~~~~~~~~'
+#print ret['photonMulti,cHadEFrac']['aoc']
+#print ret['cHadE,cHadE']['aoc']
+
+"""
+m1 = np.zeros( (len(attrL),len(attrL)) )
+#print m1
+for i in enumerate(attrL):
+    for j in enumerate(attrL):
+        a = i[1] + ',' + j[1]
+        b = j[1] + ',' + i[1]
+        for key in ret:
+            if a == key or b == key:
+                m1[i[0]][j[0]] = ret[key]['aoc'] 
+print m1
+"""
+
+imDict = {}
+for i in attrL:
+    imDict[i] = {}
+
+for i in attrL:
+    for j in attrL:
+        imDict[i][j] = 0.
+
+LL = []
+for i in attrL:
+    for j in attrL:
+        c = i+','+j
+        d = j+','+i
+        for key in ret:  
+            if c == key or d == key:   
+                imDict[i][j] = ret[key]['aoc'] 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~2bDel
+#imDict['cHadE']['cHadE'] = 0.
+#imDict['muE']['muEFrac'] = 0.54027419
+#imDict['muEFrac']['muE'] = 0.54027419
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~2bDel
+
+df = pd.DataFrame(imDict)
+print df
+LL = df.columns.values.tolist()
+
+
+
+fig, ax = plt.subplots()
+
+im, cbar = heatmap(df, LL, LL, ax=ax,
+                   cmap="YlGn", cbarlabel="AOC")
+texts = annotate_heatmap(im, valfmt="{x:.3f} t", fsize=6)
+
+fig.tight_layout()
+plt.show()
+ 
+    
+
+
+
+
+f1.close()
